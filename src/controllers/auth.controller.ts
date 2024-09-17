798eddef0741774834c5e415f88b4d609a1feb93
harshtarang7 <tarangharsh7@gmail.com>
2024-09-17 10:47:14 +0530

    created login api and route

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Request,Response } from "express";
-import User from '../models/user.model';
+import User, { IUser } from '../models/user.model';
 import { generateToken } from "../utils/jwt.utils";
 
 export const signup = async (req:Request,res:Response):Promise<void>=>{
@@ -15,13 +15,13 @@ export const signup = async (req:Request,res:Response):Promise<void>=>{
             return;
         }
         const user = await User.create({username,email,password});
-        const token = generateToken(user);
+        // const token = generateToken(user);
 
         res.status(201).json({
             _id:user._id,
             username:user.username,
             email:user.email,
-            token,
+            // token,
         });
     } catch (error) {
         const typeError = error as Error;
@@ -31,3 +31,26 @@ export const signup = async (req:Request,res:Response):Promise<void>=>{
         });
     }
 };
+
+export const login = async(req:Request,res:Response):Promise<void>=>{
+    try {
+        const {email,password} = req.body;
+      const user = await User.findOne({email});
+
+        if(user && (await user.comparePassword(password))){
+            const token = generateToken(user)
+            res.json({
+                _id:user._id,
+                username:user.username,
+                email:email,
+                token,
+             });
+        }
+        else{
+            res.status(401).json({ message: 'Invalid email or password' });
+        }
+    } catch (error) {
+        const ErrorType = error as Error;
+        res.status(500).json({message:'server Error',error:ErrorType})
+    }
+}
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,9 @@
-import express from "express";
-import { signup } from "../controllers/auth.controller";
+import express, { Router } from "express";
+import { login, signup } from "../controllers/auth.controller";
 
 const route = express.Router();
 
 route.post('/signup',signup)
+route.post('/login',login)
 
 export default route;
